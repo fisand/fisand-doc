6c71f8dabf66a6cfc011193ffbd61e070b88796d
zouhang <zouhang@didiglobal.com>
2021-08-12 20:48:20 +0800

    chore: update logo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,8 +3,18 @@ module.exports = {
   base: '/fisand-doc/',
   title: 'fisandoc',
   description: 'Vite & Vue powered static site generator.',
-
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: 'https://pt-starimg.didistatic.com/static/starimg/img/TvCSBVuy1Y1612669180297.jpeg'
+      }
+    ]
+  ],
   themeConfig: {
-    repo: 'https://github.com/zouhangwithsweet/fisand-doc'
+    repo: 'https://github.com/zouhangwithsweet/fisand-doc',
+    logo: 'https://pt-starimg.didistatic.com/static/starimg/img/TvCSBVuy1Y1612669180297.jpeg'
   }
 }

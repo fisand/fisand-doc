f0268d19a8bbbb2a184901214987d80d22736464
zouhang <zouhang@didiglobal.com>
2021-09-07 17:15:56 +0800

    chore: add some theme config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,6 +16,11 @@ module.exports = {
   themeConfig: {
     repo: 'https://github.com/zouhangwithsweet/fisand-doc',
     logo: 'https://pt-starimg.didistatic.com/static/starimg/img/TvCSBVuy1Y1612669180297.jpeg',
+    docsBranch: 'feat_fisand_doc',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '为此文档提供修改建议',
+    lastUpdated: true,
     nav: [
       {
         text: 'TypeScript',
